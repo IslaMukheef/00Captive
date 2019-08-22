9f0cc6bf3a3415f321fa25001c61253710a45197
Isla Imad <53822275+IslaIDK@users.noreply.github.com>
2019-08-22 04:51:35 -0400

    Update 00captive.py

--- 00captive.py
+++ 00captive.py
@@ -1,34 +1,49 @@
 import subprocess
 import os
 import time
+import scapy.all as scapy
 subprocess.call("ifconfig")
-interface = input("interface > ") 
-list = []
+interface = raw_input("interface > ") 
+
+
 while True:
-	f = input("enter range \nexmple:  1/24 or 100/124 >")
-	a = f.split("/")
-	if len(a) == 2:
+	f = raw_input("enter range \nexmple:  1/24 or 100/124 >")
+	if len(f) == 4:
+		break
+	if len(f) == 3:
+		break
+	if len(f) == 7:
 		break
 	else:
 		print("exmple:  1/24 or 100/124")
-for i in range(2):
-	list.append(a[i])
 
-with open('ips.txt',"w") as ip:
-	for ips in range(int(list[0]),int(list[1])):
-		address = ("192.168.1."+str(ips))
-		ip.write(address+"\n")
-
-os.system('gnome-terminal -- sudo python scan.py')
-a = subprocess.call(["awk",'/MAC Address/ {print $3}','macs.txt'])
-new_mac=input("?> ")
-
+address = "192.168.1."+f
+def scan(ip):
+	arp_packet = scapy.ARP(pdst=ip)
+	broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+	arp_broadcast_packet = broadcast_packet/arp_packet
+	answered_list = scapy.srp(arp_broadcast_packet, timeout=1, verbose=False)[0]
+	client_list = []
+	for ip_mac in answered_list:
+		client_dict = {"ip": ip_mac[1].psrc, "mac": ip_mac[1].hwsrc}
+		client_list.append(client_dict)	
+	print("IPs\t\tMACs\n---------------------------------------------------")
+	for client in client_list:
+		 print(client["ip"]+"\t"+client["mac"])
+scan(address)
+while True:
+	
+	new_mac=raw_input("New MAC ? > ")
+	if len(new_mac) == 17:
+		break
+	else:
+		print("Try again >")
 subprocess.call(["ifconfig",interface,"down"])
 subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
 subprocess.call(["ifconfig",interface,"up"])
 print("check if you still on the network")
 time.sleep(2)
-subprocess.call(["ping","www.google.com","-c3"])
+subprocess.call(["ping","-I",interface,"www.google.com","-c3"])
 
 	
 	
